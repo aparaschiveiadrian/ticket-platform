dbe21c094f3f055f8489ff226a74aac4118f7c09
aparaschiveiadrian <adrian.aparaschivei04@gmail.com>
2025-09-22 22:22:14 +0300

    mocked purchase of ticket endpoint added inside TicketTypeController & JwtUtility as utility class for converting jwt s

---
+++ ticket-service/src/main/java/com/adrianaparaschivei/ticketservice/controller/TicketTypeController.java
@@ -0,0 +1,33 @@
+package com.adrianaparaschivei.ticketservice.controller;
+
+import com.adrianaparaschivei.ticketservice.service.TicketTypeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+import static com.adrianaparaschivei.ticketservice.util.JwtUtil.parseUserId;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/events/{eventId}/ticket-types")
+public class TicketTypeController {
+
+  private final TicketTypeService ticketTypeService;
+
+  @PostMapping("/{ticketTypeId}/tickets")
+  public ResponseEntity<Void> purchaseTicket(
+          @AuthenticationPrincipal Jwt jwt,
+          @PathVariable UUID ticketTypeId
+  ){
+    ticketTypeService.purchaseTicket(parseUserId(jwt), ticketTypeId);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+}

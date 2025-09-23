92b7945cdd1a99142219a3c544786c3aa92a4dab
aparaschiveiadrian <adrian.aparaschivei04@gmail.com>
2025-09-23 17:12:22 +0300

    get QR code Endpoint along with full functionality flow, decoding in base64 in byte arr

--- ticket-service/src/main/java/com/adrianaparaschivei/ticketservice/repository/QrCodeRepository.java
+++ ticket-service/src/main/java/com/adrianaparaschivei/ticketservice/repository/QrCodeRepository.java
@@ -3,6 +3,9 @@ package com.adrianaparaschivei.ticketservice.repository;
 import com.adrianaparaschivei.ticketservice.model.entity.QrCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {}
+public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {
+  Optional<QrCode> findByTicketIdAndTicketPurchaserId(UUID ticketId, UUID ticketPurchaseId);
+}
--- ticket-service/src/main/java/com/adrianaparaschivei/ticketservice/service/QrCodeService.java
+++ ticket-service/src/main/java/com/adrianaparaschivei/ticketservice/service/QrCodeService.java
@@ -4,7 +4,11 @@ import com.adrianaparaschivei.ticketservice.model.entity.QrCode;
 import com.adrianaparaschivei.ticketservice.model.entity.Ticket;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface QrCodeService {
   QrCode generateQrCodeForTicket(Ticket ticket);
+
+  byte[] getQrCodeImageForUserAndTicket(UUID userId, UUID ticketId);
 }
